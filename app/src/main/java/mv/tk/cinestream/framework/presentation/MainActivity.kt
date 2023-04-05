cdfe8808a486ff472fcdd8fde71e2eed811bcb5e
tk-code-dev <reiwa.dupuis.2019@gmail.com>
2023-04-05 15:40:26 -0400

    avoid calling same tab

--- app/src/main/java/mv/tk/cinestream/framework/presentation/MainActivity.kt
+++ app/src/main/java/mv/tk/cinestream/framework/presentation/MainActivity.kt
@@ -1,14 +1,12 @@
 package mv.tk.cinestream.framework.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavOptions
 import androidx.navigation.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import mv.tk.cinestream.R
 import mv.tk.cinestream.databinding.ActivityMainBinding
-import mv.tk.cinestream.util.Constants.TAG
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -21,25 +19,39 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.bottomNavigationView.setOnItemSelectedListener { menuItem ->
-            Log.d(TAG,menuItem.toString())
 
-            when (menuItem.itemId) {
-                R.id.movieTab -> {
-                    findNavController(R.id.nav_host_main).navigate(R.id.movieFragment,null,NavOptions.Builder()
-                        .setEnterAnim(R.anim.slide_from_left).setExitAnim(R.anim.slide_to_right).build())
-                    true
+            val tabPosition = binding.bottomNavigationView.selectedItemId
+            //  Avoid calling the same tab repeatedly
+            if (tabPosition != menuItem.itemId) {
+                when (menuItem.itemId) {
+                    R.id.movieTab -> {
+                        findNavController(R.id.nav_host_main).navigate(
+                            R.id.movieFragment, null, NavOptions.Builder()
+                                .setEnterAnim(R.anim.slide_from_left)
+                                .setExitAnim(R.anim.slide_to_right).build()
+                        )
+                        true
+                    }
+                    R.id.tvTab -> {
+                        findNavController(R.id.nav_host_main).navigate(
+                            R.id.tvFragment, null, NavOptions.Builder()
+                                .setEnterAnim(R.anim.slide_from_left)
+                                .setExitAnim(R.anim.slide_to_right).build()
+                        )
+                        true
+                    }
+                    R.id.actorTab -> {
+                        findNavController(R.id.nav_host_main).navigate(
+                            R.id.actorFragment, null, NavOptions.Builder()
+                                .setEnterAnim(R.anim.slide_from_left)
+                                .setExitAnim(R.anim.slide_to_right).build()
+                        )
+                        true
+                    }
+                    else -> false
                 }
-                R.id.tvTab -> {
-                    findNavController(R.id.nav_host_main).navigate(R.id.tvFragment,null,NavOptions.Builder()
-                        .setEnterAnim(R.anim.slide_from_left).setExitAnim(R.anim.slide_to_right).build())
-                    true
-                }
-                R.id.actorTab -> {
-                    findNavController(R.id.nav_host_main).navigate(R.id.actorFragment,null,NavOptions.Builder()
-                        .setEnterAnim(R.anim.slide_from_left).setExitAnim(R.anim.slide_to_right).build())
-                    true
-                }
-                else -> false
+            } else {
+                false
             }
         }
     }
